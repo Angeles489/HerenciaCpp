534a0355f00bde1a2106aac263432ea50bd7aa65
Angeles Araiza <angelesaragar@hotmail.com>
2025-05-19 12:34:33 -0600

    Update ejercicio_01.cpp

--- Herencia_Angeles_araiza/ejercicio_01.cpp
+++ Herencia_Angeles_araiza/ejercicio_01.cpp
@@ -1,1 +1,35 @@
+// Ejercicio 1
+
+#include <iostream>
+using namespace std;
+
+// Completa aquí las clases:
+
+class Animal {
+public:
+    string nombre;
+    Animal(string _nom) {
+        nombre=_nom;
+    }
+    void comer() {
+        cout << "El animal " << nombre<<" come"<< endl;
+    }
+};
+
+class Perro : public Animal {
+public:
+    Perro(string nombre):Animal(nombre) {}
+
+    void ladrar() {
+        cout << "El "<<nombre<< " esta ladrando" << endl;
+    }
+};
+
+int main() {
+
+    Perro miPerro("Firulais");
+    miPerro.comer();
+    miPerro.ladrar();
+    return 0;
+}
 
